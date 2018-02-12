3795085fb177c9b5b9ebd072b102f9bdf031f83b
Kyle Bebak <kylebebak@gmail.com>
2018-02-12 09:16:26 -0600

    Changes deploy script to upload files to S3 bucket that will serve site

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Development
 `npm install`, then `npm run dev`. Then open `index.html` in your browser.
+
+## Deploy
+`npm run deploy`: because it's entirely static, our landing page is hosted by S3. Deployment uploads necessary static files to the bucket that serves the site.
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
+# `aws` command depends on credentials in `~/.aws` directory
+# aws s3 ls s3://719s.mx
 ./build.sh
+bucketname='719s.mx'
 
-scp -i ~/.ssh/919mx public/styles/global.css custom@52.37.92.55:/var/www/919/web/styles/global.css
-
-scp -i ~/.ssh/919mx -r public/index.html custom@52.37.92.55:/var/www/919/web
-scp -i ~/.ssh/919mx -r public/assets custom@52.37.92.55:/var/www/919/web
+aws s3 cp public/index.html s3://${bucketname} --acl public-read
+aws s3 cp public/styles/global.css s3://${bucketname}/styles/ --acl public-read
+aws s3 sync public/assets s3://${bucketname}/assets --acl public-read
