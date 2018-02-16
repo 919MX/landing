dd6f51c111ca6904e90dfb2eface17eb4125b2a9
Kyle Bebak <kylebebak@gmail.com>
2018-02-16 12:12:40 -0600

    nosotros.html -> nosotros, and site serves it at this URL, without .html extension

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ bucketname='ensintonia.org'
 
 aws s3 cp public/index.html s3://${bucketname} --acl public-read
 aws s3 cp public/error.html s3://${bucketname} --acl public-read
+aws s3 cp public/nosotros s3://${bucketname} --acl public-read --content-type "text/html"
 aws s3 cp public/styles/global.css s3://${bucketname}/styles/ --acl public-read
 aws s3 sync public/assets s3://${bucketname}/assets --acl public-read
