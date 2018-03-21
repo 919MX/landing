a4948e6b6f97b073c89de728638b4a4421289341
Kyle Bebak <kylebebak@gmail.com>
2018-03-21 12:15:13 -0600

    Adds robots.txt file

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,13 @@ bucketname='brigada.mx'
 
 aws s3 cp public/index.html s3://${bucketname} --acl public-read
 aws s3 cp public/error.html s3://${bucketname} --acl public-read
+
 aws s3 cp public/nosotros s3://${bucketname} --acl public-read --content-type "text/html"
 aws s3 cp public/privacidad s3://${bucketname} --acl public-read --content-type "text/html"
 aws s3 cp public/terminos s3://${bucketname} --acl public-read --content-type "text/html"
+
 aws s3 cp public/livechat.js s3://${bucketname} --acl public-read
 aws s3 cp public/styles/global.css s3://${bucketname}/styles/ --acl public-read
 aws s3 sync public/assets s3://${bucketname}/assets --acl public-read
+
+aws s3 cp public/robots.txt s3://${bucketname} --acl public-read
