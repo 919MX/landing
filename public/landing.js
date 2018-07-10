07d1a3accd83b6ce125197ec50eda19979960300
Kyle Bebak <kylebebak@gmail.com>
2018-07-10 17:33:08 -0500

    Adds section headers and renderCommunitiesHeader function

--- public/landing.js
+++ public/landing.js
@@ -102,6 +102,11 @@ const renderMetrics = ({ groups, actions, total_spent: spent }) => {
   _p.innerHTML = metricsStr
 }
 
+const renderCommunitiesHeader = ({ results }) => {
+  const _h = document.getElementById('brigada-communities-header')
+  _h.innerHTML = `Al dia de hoy, estamos en ${results.length} comunidades`
+}
+
 const renderActions = ({ results }) => {
   const _container = document.getElementById('brigada-actions')
 
@@ -231,6 +236,8 @@ const renderOpportunities = ({ results }) => {
 const render = (data) => {
   const { metrics, actions, opportunities, localities } = data
   renderMetrics(metrics)
+  renderCommunitiesHeader(localities)
+
   renderActions(actions)
   renderOpportunities(opportunities)
 
